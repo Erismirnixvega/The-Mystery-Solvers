c32b209a51d94a0ae38703c629d77432c44024c5
Erismirnixvega <9023832@student.zadkine.nl>
2026-05-21 14:45:35 +0200

    3 Riddles added to the database (Arda)

--- sql/riddles.sql
+++ sql/riddles.sql
@@ -12,7 +12,7 @@ VALUES
     ('', '', '', '1'),
     ('', '', '', '1'),
     ('', '', '', '1'),
-    ('', '', '', '2'),
-    ('', '', '', '2'),
-    ('', '', '', '2');
+    ('Het bos is behoorlijk gevuld met mysterieuze wegen. Welk pad is blijkbaar het veiligst weg?', 'Bezaanmast Mangroven', 'Een bosrijke kustlijn waar verwoeste rompen aanspoelen.', '2'),
+    ('Welke kist verbergt de sleutel?', 'XIV', 'Een antieke uitstraling. De patronen zijn zo fraai ontworpen dat ze vooral bij de adel in de smaak vallen.', '2'),
+    ('Je hebt twee stukken gereedschap gevonden die rondslingerden. Welke van deze lijkt op de lange termijn nuttig te zijn??', 'Samen', 'Er is een gezegde dat luidt: Wees altijd voorbereid door iets achter de hand te houden', '2');
 
