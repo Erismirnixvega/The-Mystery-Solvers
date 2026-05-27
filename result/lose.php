f9cf91acaa0d7a4b1f956e5086b6943d1b4aebfb
Erismirnixvega <9023832@student.zadkine.nl>
2026-05-27 22:50:54 +0200

    styling added to css

--- result/lose.php
+++ result/lose.php
@@ -16,9 +16,10 @@ $naam = htmlspecialchars($naam, ENT_QUOTES, 'UTF-8');
 <!DOCTYPE html>
 <html lang="nl">
 <head>
+    <link rel="stylesheet" href="../css/result.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pirata+One&family=Trade+Winds&display=swap" rel="stylesheet">
--- result/win.php
+++ result/win.php
@@ -21,7 +21,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Overwinning</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/result.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pirata+One&family=Trade+Winds&display=swap" rel="stylesheet">
